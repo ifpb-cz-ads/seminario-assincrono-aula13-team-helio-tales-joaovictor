6489963b5f1aa158a7f91644228e696ee8314961
Tales-Anderson <70909134+Tales-Anderson@users.noreply.github.com>
2020-12-12 19:37:37 -0300

    Exemplo sem as expressões lambda

--- Main.java
+++ Main.java
@@ -2,6 +2,28 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+         List<Produto> list = new ArrayList<>();
+
+        list.add(new Produto("TV", 900.00));
+        list.add(new Produto("Gabinete", 200.00));
+        list.add(new Produto("Notebook", 1200.00));
+        list.add(new Produto("Tablet", 450.00));
+
+
+
+
+        Comparator<Produto> comp = new Comparator<Produto>() {
+            @Override
+            public int compare(Produto p1, Produto p2) {
+                return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+            }
+        };
+
+        list.sort(comp);
+
+        for (Produto p : list) {
+            System.out.println(p);
+        }
     }
 }
 
